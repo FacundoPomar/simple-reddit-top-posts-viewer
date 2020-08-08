958a57d63717274dfb05c4134271161ead0d6a35
Facundo Pomar <facundo.pomar@globallogic.com>
2020-08-08 16:49:39 -0300

    * Installer material * Removed demo app content * Added basic app content with material usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
 import './App.css';
+import { Typography } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app__container'>
+      <div>
+        <Typography variant='h1'>
+          Showing Material
+        </Typography>
+      </div>
+      <div className='app__logo-container'>
+        <AllInclusiveIcon fontSize='inherit' />
+      </div>
     </div>
   );
 }
