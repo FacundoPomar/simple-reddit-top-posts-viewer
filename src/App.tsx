4e83d67bdcfc3b4492d416396d93b52b83c223b2
Facundo Pomar <facundo.pomar@globallogic.com>
2020-08-09 19:10:09 -0300

    * Moved posts loading logic to PostList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.scss';
 import { useSelector } from 'react-redux';
-import postsStore, { PostsState } from './stores/posts';
-import { postsLoaded, postsLoading, postSelected } from './actions/posts';
-import PostItemLoading from './components/PostItemLoading/PostItemLoading';
+import { PostsState } from './stores/posts';
 import AppNavbar from './components/AppNavbar/AppNavbar';
 import PostsList from './components/PostsList/PostsList';
-import PostsService, { PostsFetchRequest } from './Services/Posts';
-import { Button } from '@material-ui/core';
-import { PostsSlice, Post } from './models/post';
-import PostItemLayout from './components/PostItemLayout/PostItemLayout';
+import { Post } from './models/post';
 import PostDetails from './components/PostDetail/PostDetail';
 
 const App: React.FC = () => {
-  const dataLoading = useSelector<PostsState, boolean>( state => state.postsLoading );
-  const currentSlice = useSelector<PostsState, PostsSlice>( state => state.slice );
   const selectedPost = useSelector<PostsState, Post | undefined>( state => state.selectedPost );
-
-  useEffect( () => {
-    loadPosts();
-  }, []);
-
-  const loadPosts = async ( options?: PostsFetchRequest ) => {
-    postsStore.dispatch( postsLoading( true ) );
-
-    const postsResponse = await PostsService.loadPosts( options );
-
-    const posts = postsResponse.data.children.map( child => child.data );
-    const newSlice = postsResponse.data.after || '';
-
-    postsStore.dispatch( postsLoaded( posts, newSlice ) );
-
-    postsStore.dispatch( postsLoading( false ) );
-  };
-
-  const onPrev = () => {
-    loadPosts({
-      before: currentSlice.current
-    });
-  };
-
-  const onNext = () => {
-    loadPosts({
-      after: currentSlice.current
-    });
-  };
-
-  const onPostSelection = ( post: Post ): void => {
-    postsStore.dispatch( postSelected( post ) );
-  }
 
   return (
     <div className='app__container'>
       <AppNavbar />
       <div className='app__inner-container'>
         <div className='app__posts-container'>
-          { dataLoading &&
-            <>
-              <PostItemLoading />
-              <PostItemLoading />
-              <PostItemLoading />
-            </>
-          }
-          { !dataLoading &&
-            <>
-            <PostsList onSelection={onPostSelection}/>
-            <PostItemLayout>
-              <div className='app__posts-pagination'>
-                <Button
-                  variant='contained'
-                  color='secondary'
-                  onClick={onPrev}>
-                  Prev
-                </Button>
-                <Button
-                  variant='contained'
-                  color='secondary'
-                  onClick={onNext}>
-                  Next
-                </Button>
-              </div>
-            </PostItemLayout>
-            </>
-          }
+          <PostsList />
         </div>
         <div className='app__post-detail-container'>
           <PostDetails post={selectedPost}/>
--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import { Post } from '../../models/post';
+import React, { useEffect } from 'react';
+import { Post, PostsSlice } from '../../models/post';
 import { useSelector } from 'react-redux';
 import postsStore, { PostsState } from '../../stores/posts';
 import PostItem from '../PostItem/PostItem';
 import PostItemLayout from '../PostItemLayout/PostItemLayout';
-import { Typography } from '@material-ui/core';
-import { postDismissed } from '../../actions/posts';
+import { Typography, Button } from '@material-ui/core';
+import { postDismissed, postSelected, postsLoading, postsLoaded } from '../../actions/posts';
+import PostsService, { PostsFetchRequest } from '../../Services/Posts';
+import './PostsList.scss';
+import PostItemLoading from '../PostItemLoading/PostItemLoading';
 
-export interface PostsListProps {
-  onSelection: ( post: Post ) => void;
-}
-
-const PostsList: React.FC<PostsListProps> = ({ onSelection }) => {
+const PostsList: React.FC = () => {
   const posts = useSelector<PostsState, Post[]>( state => state.posts );
   const selectedPost = useSelector<PostsState, Post | undefined>( state => state.selectedPost );
+  const currentSlice = useSelector<PostsState, PostsSlice>( state => state.slice );
+  const dataLoading = useSelector<PostsState, boolean>( state => state.postsLoading );
+
+  useEffect( () => {
+    loadPosts();
+  }, []);
+
+  const loadPosts = async ( options?: PostsFetchRequest ) => {
+    postsStore.dispatch( postsLoading( true ) );
+
+    const postsResponse = await PostsService.loadPosts( options );
+
+    const posts = postsResponse.data.children.map( child => child.data );
+    const newSlice = postsResponse.data.after || '';
+
+    postsStore.dispatch( postsLoaded( posts, newSlice ) );
+
+    postsStore.dispatch( postsLoading( false ) );
+  };
+
+  const onPrev = () => {
+    loadPosts({
+      before: currentSlice.current
+    });
+  };
+
+  const onNext = () => {
+    loadPosts({
+      after: currentSlice.current
+    });
+  };
 
   const isPostSelected = ( post: Post ): boolean => {
     return post.id === selectedPost?.id;
@@ -23,27 +53,54 @@ const PostsList: React.FC<PostsListProps> = ({ onSelection }) => {
     postsStore.dispatch( postDismissed( post ) );
   };
 
+  const onPostSelection = ( post: Post ): void => {
+    postsStore.dispatch( postSelected( post ) );
+  }
+
   return (
     <>
-      { !posts.length &&
+      { (!posts.length && !dataLoading) &&
         <PostItemLayout>
           <Typography variant='subtitle1'>
             No posts to show.
           </Typography>
         </PostItemLayout>
       }
-      { posts.map( post =>
+      { dataLoading &&
+        <>
+          <PostItemLoading />
+          <PostItemLoading />
+          <PostItemLoading />
+        </>
+          }
+      { !dataLoading && posts.map( post =>
         <PostItem
           key={post.id}
           selected={isPostSelected( post )}
           onClick={(): void => {
-            onSelection( post );
+            onPostSelection( post );
           }}
           onDismiss={(): void => {
             onDismiss( post );
           }}
           post={post} />
       )}
+      <PostItemLayout>
+        <div className='posts-list__posts-pagination'>
+          <Button
+            variant='contained'
+            color='secondary'
+            onClick={onPrev}>
+            Prev
+          </Button>
+          <Button
+            variant='contained'
+            color='secondary'
+            onClick={onNext}>
+            Next
+          </Button>
+        </div>
+      </PostItemLayout>
     </>
   );
 };
